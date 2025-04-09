6978ce4e80039721587f22c9b14b829a44905e23
Seharo <pcu8757@gmail.com>
2025-04-09 16:02:56 +0900

    20250409

---
+++ Chap4/ch04-01.c
@@ -0,0 +1,29 @@
+//학번 + 이름 : 202511220 박찬웅
+//프로그램명 : 질량과 속도를 입력받아 운동에너지를 계산하는 프로그램
+//작성일 : 20250409
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+void kineticenergy(void);
+
+int main(void)
+{
+	kineticenergy();
+
+	return 0;
+}
+
+void kineticenergy(void)
+{
+	double mass = 0.0, velocity = 0.0, energy = 0.0;
+
+	scanf("%lf %lf", &mass, &velocity);
+	energy = 0.5 * mass * velocity * velocity;
+	
+	printf("%lf\n", energy);
+
+
+	return;
+}
